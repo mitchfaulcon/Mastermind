4143b20acb4b88b4357449e074aba7f8d6781140
mitchfaulcon <42291691+mitchfaulcon@users.noreply.github.com>
2018-10-27 16:36:33 +1300

    Create top10times text file populated with dummy data

--- src/mastermind/Main.java
+++ src/mastermind/Main.java
@@ -6,9 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main extends Application {
 
     private static Stage _stage;
+    private final String leaderBoardFileName = "Top10Times.txt";
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -22,6 +28,8 @@ public class Main extends Application {
 //            shutdown();
 //        });
 
+
+        //Change to home screen
         Parent root = FXMLLoader.load(getClass().getResource("view/Home.fxml"));
         primaryStage.setTitle("MasterMind");
         primaryStage.setScene(new Scene(root, 750, 500));
@@ -29,6 +37,8 @@ public class Main extends Application {
         primaryStage.setResizable(false);
 
         _stage = primaryStage;
+
+        createLeaderboardFile();
     }
 
 
@@ -36,10 +46,50 @@ public class Main extends Application {
         launch(args);
     }
 
-    //change scene
     public static void ChangeScene(Scene scene) {
+        //Change to input scene
         _stage.setScene(scene);
         _stage.sizeToScene();
+    }
+
+    private void createLeaderboardFile(){
+        File leaderboardFile = new File(leaderBoardFileName);
+
+        try {
+            //Creates 'Top10Times.txt' file if it does not exist
+            if (leaderboardFile.createNewFile()){
+                //If file did not exist, populate with dummy data
+                populateLeaderboard();
+            }
+        } catch (IOException e) {
+
+        }
+    }
+
+    private void populateLeaderboard(){
+
+        try {
+            //Writer to write to leaderboard txt file
+            FileWriter fileWriter = new FileWriter(leaderBoardFileName);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            for (int i=1;i<=10;i++){
+                String entry = "Player " + i + "\t0:" + (i+9) + ":00";
+                bufferedWriter.write(entry);
+
+                //Add newline char if not last entry
+                if (i<10){
+                    bufferedWriter.write("\n");
+                }
+            }
+
+            //Close files
+            bufferedWriter.close();
+            fileWriter.close();
+        }
+        catch (IOException ex){
+
+        }
     }
 
     //    private void shutdown(){
