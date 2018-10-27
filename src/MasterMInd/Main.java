7784de29e72a2944af67ebab327e8c98ccad8f0d
mitchfaulcon <42291691+mitchfaulcon@users.noreply.github.com>
2018-10-27 13:36:50 +1300

    Added commented out quit confirmation

--- src/MasterMInd/Main.java
+++ src/MasterMInd/Main.java
@@ -4,12 +4,24 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+//        //set event handler for when user presses 'x' button on stage.
+//        primaryStage.setOnCloseRequest(evt -> {
+//            // prevent window from closing
+//            evt.consume();
+//
+//            // execute own shutdown procedure
+//            shutdown();
+//        });
+
         Parent root = FXMLLoader.load(getClass().getResource("View/Home.fxml"));
         primaryStage.setTitle("MasterMind");
         primaryStage.setScene(new Scene(root, 300, 275));
@@ -21,5 +33,13 @@ public class Main extends Application {
         launch(args);
     }
 
+
+//    private void shutdown(){
+//        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to quit MasterMind?", ButtonType.YES, ButtonType.NO);
+//        if (alert.showAndWait().orElse(ButtonType.NO) == ButtonType.YES) {
+//            //Quit
+//            System.exit(1);
+//        }
+//    }
 }
 
